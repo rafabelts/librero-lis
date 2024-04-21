ed1d8fa81226be8764db102d617e7d517f119f35
Rafael Beltran <rafabeltrans17@gmail.com>
2024-04-20 20:34:27 -0600

    Changed end point names

--- server/index.js
+++ server/index.js
@@ -24,31 +24,31 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cors(corsOptions));
 
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1>Hola!</h1>");
 })
 
 
-app.get("/api/librero-lis", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("<h1>API Librero LIS</h1>")
 });
 
-app.post("/api/librero-lis/auth/sign-up", handleSignUp);
-app.post("/api/librero-lis/auth/log-in", handleLogIn);
-app.post("/api/librero-lis/change-name", changeName);
-app.post("/api/librero-lis/send-recovery-email", sendEmailToRecoverPassword);
-app.post("/api/librero-lis/change-password", changePassword);
+app.post("/api/auth/sign-up", handleSignUp);
+app.post("/api/auth/log-in", handleLogIn);
+app.post("/api/change-name", changeName);
+app.post("/api/send-recovery-email", sendEmailToRecoverPassword);
+app.post("/api/change-password", changePassword);
 
-app.get("/api/librero-lis/user-session-status", userSessionStatus);
-app.get("/api/librero-lis/sign-out", signOut)
+app.get("/api/user-session-status", userSessionStatus);
+app.get("/api/sign-out", signOut)
 
-app.get("/api/librero-lis/fetch-books-data", fetchBooksData);
-app.get("/api/librero-lis/fetch-books-on-loan-data", fetchBooksOnLoanData);
-app.get("/api/librero-lis/fetch-students-data", fetchStudentsData);
+app.get("/api/fetch-books-data", fetchBooksData);
+app.get("/api/fetch-books-on-loan-data", fetchBooksOnLoanData);
+app.get("/api/fetch-students-data", fetchStudentsData);
 
-app.post("/api/librero-lis/books/add", addBook);
-app.post("/api/librero-lis/books/return-book", returnToInventory);
-app.post("/api/librero-lis/books/add-loan", addBookToLoan);
+app.post("/api/books/add", addBook);
+app.post("/api/books/return-book", returnToInventory);
+app.post("/api/books/add-loan", addBookToLoan);
 
 app.listen(port);
 
