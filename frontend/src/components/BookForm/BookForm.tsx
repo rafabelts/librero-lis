6a6f06588681f726b7a3be863467d2d062305f28
Rafael Beltran <rafabeltrans17@gmail.com>
2024-08-12 17:38:00 -0600

    Fixed loading toast behavior

--- frontend/src/components/BookForm/BookForm.tsx
+++ frontend/src/components/BookForm/BookForm.tsx
@@ -10,90 +10,95 @@ import { addBookService } from '../../services/bookServices.ts';
 import { toast, Toaster } from 'sonner';
 
 export function AddBookForm() {
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        formState: { errors },
-    } = useForm<z.infer<typeof BookSchema>>({
-        resolver: zodResolver(BookSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<z.infer<typeof BookSchema>>({
+    resolver: zodResolver(BookSchema),
+  });
 
-    const onSubmit = (data: BookFormData) => {
-        toast.loading('Añadiendo libro...');
-        addBookService(data);
-    };
+  const onSubmit = async (data: BookFormData) => {
+    const loadingToast = toast.loading('Añadiendo libro...');
 
-    const bookFields: Array<FormFieldProps<BookFormData>> = [
-        {
-            type: 'text',
-            placeholder: 'ISBN',
-            name: 'isbn',
-            register: register,
-            error: errors.isbn,
-        },
-        {
-            type: 'text',
-            placeholder: 'Título del libro',
-            name: 'title',
-            register: register,
-            error: errors.title,
-        },
-        {
-            type: 'text',
-            placeholder: 'Autor(es) del libro',
-            name: 'author',
-            register: register,
-            error: errors.author,
-        },
-        {
-            type: 'text',
-            placeholder: 'Editorial',
-            name: 'editorial',
-            register: register,
-            error: errors.editorial,
-        },
-        {
-            type: 'number',
-            placeholder: 'Año de publicación',
-            name: 'publicationYear',
-            register: register,
-            error: errors.publicationYear,
-            valueAsNumber: true,
-        },
-        {
-            type: 'number',
-            placeholder: 'Copias disponibles',
-            name: 'copies',
-            register: register,
-            error: errors.copies,
-            valueAsNumber: true,
-        },
-    ];
+    try {
+      await addBookService(data);
+    } finally {
+      toast.dismiss(loadingToast);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className="formContainer">
-            <div className={styles.inputGrid}>
-                {bookFields.map((field) => (
-                    <FormField
-                        key={field.name}
-                        type={field.type}
-                        placeholder={field.placeholder}
-                        name={field.name}
-                        register={field.register}
-                        error={field.error}
-                        valueAsNumber={field.valueAsNumber}
-                    />
-                ))}
-            </div>
-            <BookImageSelector
-                setValue={setValue}
-                register={register}
-                error={errors.bookImage}
-            />
+  const bookFields: Array<FormFieldProps<BookFormData>> = [
+    {
+      type: 'text',
+      placeholder: 'ISBN',
+      name: 'isbn',
+      register: register,
+      error: errors.isbn,
+    },
+    {
+      type: 'text',
+      placeholder: 'Título del libro',
+      name: 'title',
+      register: register,
+      error: errors.title,
+    },
+    {
+      type: 'text',
+      placeholder: 'Autor(es) del libro',
+      name: 'author',
+      register: register,
+      error: errors.author,
+    },
+    {
+      type: 'text',
+      placeholder: 'Editorial',
+      name: 'editorial',
+      register: register,
+      error: errors.editorial,
+    },
+    {
+      type: 'number',
+      placeholder: 'Año de publicación',
+      name: 'publicationYear',
+      register: register,
+      error: errors.publicationYear,
+      valueAsNumber: true,
+    },
+    {
+      type: 'number',
+      placeholder: 'Copias disponibles',
+      name: 'copies',
+      register: register,
+      error: errors.copies,
+      valueAsNumber: true,
+    },
+  ];
 
-            <button className="appButton">Agregar</button>
-            <Toaster />
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="formContainer">
+      <div className={styles.inputGrid}>
+        {bookFields.map((field) => (
+          <FormField
+            key={field.name}
+            type={field.type}
+            placeholder={field.placeholder}
+            name={field.name}
+            register={field.register}
+            error={field.error}
+            valueAsNumber={field.valueAsNumber}
+          />
+        ))}
+      </div>
+      <BookImageSelector
+        setValue={setValue}
+        register={register}
+        error={errors.bookImage}
+      />
+
+      <button className="appButton">Agregar</button>
+      <Toaster />
+    </form>
+  );
 }
